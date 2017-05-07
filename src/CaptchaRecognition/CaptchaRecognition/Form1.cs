a7998fe98c2b69df6b4b2bac4761e6477bbc4bcd
Alexander Vasilenko <a6461@yandex.ru>
2017-05-07 13:48:25 +0300

    First captcha and 5/6 second recognized

--- src/CaptchaRecognition/CaptchaRecognition/Form1.cs
+++ src/CaptchaRecognition/CaptchaRecognition/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using Emgu.CV;
+using Emgu.CV.OCR;
 using Emgu.CV.Structure;
 using System;
 using System.Drawing;
@@ -21,19 +22,21 @@ namespace CaptchaRecognition
         {
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                //pictureBox1.Image = Image.FromFile(ofd.FileName);
                 image = new Image<Bgr, byte>(ofd.FileName);
                 invimage = image;
-                //Image<Bgr, Byte> invImage = null;
                 image = image.ThresholdBinary(new Bgr(100, 100, 100), new Bgr(255, 255, 255));
                 CvInvoke.BitwiseNot(image, invimage);
-                //var grayImage = image.Convert<Gray, Byte>();
                 CvInvoke.MorphologyEx(invimage, image, Emgu.CV.CvEnum.MorphOp.Dilate,
                   CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Ellipse, new Size(3, 3), new Point(-1, -1)),
                   new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Default, new MCvScalar());
-                //pictureBox1.Image = grayImage.ToBitmap();
                 pictureBox1.Image = image.ToBitmap();
                 pictureBox1.Invalidate();
+
+                Tesseract t = new Tesseract();
+                t.SetVariable("tessedit_char_whitelist", "abcdefghijklmnopqrstuvwxyz0123456789");
+                t.Init(@"C:\Emgu\emgucv-windesktop 3.1.0.2504\Emgu.CV.World\tessdata", "eng", OcrEngineMode.Default);
+                t.Recognize(image);
+                textBox1.Text = t.GetText();
             }
         }
     }
